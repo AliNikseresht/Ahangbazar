9df0af243e52da880c8e4ad2d765b881b552fa3a
Ali <alinksht@gmail.com>
2025-07-28 13:52:58 +0330

    add Google Analytics in layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,11 +59,11 @@ export default function RootLayout({
         />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
-          `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-T9WXMWV5YR');
+  `}
         </Script>
       </head>
       <body
